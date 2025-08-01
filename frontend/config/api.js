ea8f9da379289fc2ee082f0868b64e4bf0ecad2e
Asif Shaikh <asifsk.5070@gmail.com>
2025-08-01 16:10:51 +0530

    Update api.js

--- frontend/config/api.js
+++ frontend/config/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'http://192.161.132.105:5000/api', // replace with your IP + backend port
+  baseURL: 'http://192.161.132.105:5000/api'// Actual url use karna hai.. this is for development only
 });
 
 export default instance;
